985b8b7142f5c2bc52556274d19b83e3547a37fb
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-03-23 14:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(s){
+  s = s.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  
+  // compare the original string with the reversed string
+  return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
